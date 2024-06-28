3a08428b157541bff6cea40266269ce6e853a1a9
Marcel Flores <127535882+MarcelFlrs@users.noreply.github.com>
2024-06-28 09:42:05 -0300

    criação aluno

---
+++ Alunos/src/br/edu/up/Program.java
@@ -0,0 +1,12 @@
+package br.edu.up;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("\n--- Prova A1 ---");
+
+        
+    }
+}
