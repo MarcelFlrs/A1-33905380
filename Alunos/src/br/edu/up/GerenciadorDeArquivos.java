9499a50a2b0d915e5212fa64af698a199f67e705
Marcel Flores <127535882+MarcelFlrs@users.noreply.github.com>
2024-06-28 09:49:48 -0300

    gerenciador leitor gravador

---
+++ Alunos/src/br/edu/up/GerenciadorDeArquivos.java
@@ -0,0 +1,61 @@
+package br.edu.up;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GerenciadorDeArquivos {
+    private String header = "";
+    private String arquivoAlunos = "C:\\Users\\autologon\\Documents\\GitHub\\A1-33905380\\Alunos\\src\\br\\edu\\up\\alunos.csv";
+    private String arquivoResumo = "C:\\Users\\autologon\\Documents\\GitHub\\A1-33905380\\Alunos\\src\\br\\edu\\up\\resumo.csv";
+
+    public List<Aluno> getAlunos() {
+        List<Aluno> listaDeAlunos = new ArrayList<>();
+
+        try (Scanner leitor = new Scanner(new File(arquivoAlunos))) {
+
+            if (leitor.hasNextLine()) {
+                leitor.nextLine();
+            }
+            while (leitor.hasNextLine()) {
+                String linha = leitor.nextLine();
+                String dados[] = linha.split(";");
+
+                String matricula = dados[0];
+                String nome = dados[1];
+                String nota = dados[2];
+
+                Aluno aluno = new Aluno(matricula, nome, nota);
+                listaDeAlunos.add(aluno);
+
+            }
+        } catch (Exception e) {
+            System.out.println("Arquivo de alunos não encontrado! " + e.getMessage());
+        }
+
+        return listaDeAlunos;
+    }
+
+    public boolean gravarAlunoLista(List<Aluno> alunos) {
+        try {
+            FileWriter arquivoGravar = new FileWriter(arquivoResumo);
+            PrintWriter gravador = new PrintWriter(arquivoGravar);
+
+            gravador.println("matricula;nome;nota");
+            for (Aluno aluno : alunos) {
+                gravador.println(aluno.toCSV());
+            }
+
+            gravador.close();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Não foi possível gravar o arquivo!");
+        }
+        return false;
+    }
+
+}
--- Alunos/src/br/edu/up/Program.java
+++ Alunos/src/br/edu/up/Program.java
@@ -7,6 +7,24 @@ public class Program {
 
         System.out.println("\n--- Prova A1 ---");
 
+        GerenciadorDeArquivos gArquivos = new GerenciadorDeArquivos();
+
+        List<Aluno> listaAlunos;
+        listaAlunos = gArquivos.getAlunos();
+
+        System.out.println("\nLista de Alunos: ");
+
+        if (listaAlunos.size() == 0) {
+            System.out.println("Não há Alunos cadastrados");
+        } else {
+            System.out.println(listaAlunos.toString());
+        }
+
+        if (gArquivos.gravarAlunoLista(listaAlunos) == true) {
+            System.out.println("Arquivo Gravado com sucesso!");
+        }else{
+            System.out.println("Falha ao gravar arquivo");
+        }
         
     }
 }
